c6516986f99acd72b63bb637c2097d7608fd3e79
Layla <laylab@skilstak.sh>
2016-10-24 18:34:57 -0400

    save

--- dodge/game.js
+++ dodge/game.js
@@ -1,11 +1,23 @@
-var game = new Phaser.Game(320,568);
+gamenew Phaser.Game(320,568);
 class Boot {
   preload() {
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
   }
+  create() {
+    this.state.start("Load");
+  }
 }
 
+class Load {
+  preload() {
+    console.log("Loading...")
+  }
+  create() {
+    console.log("Loaded");
+  }
+}
 game.state.add("Boot",Boot);
+game.state.add("Load",Load);
 game.state.start("Boot");
